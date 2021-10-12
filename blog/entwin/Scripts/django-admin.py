95d18fc4c80fc158dd583def6f1c3a13f60c7c77
ccortesp89 <62478671+ccortesp89@users.noreply.github.com>
2021-10-12 18:57:35 -0300

    entwin

---
+++ blog/entwin/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\claudio\desktop\proyectosgit\pagnayidjango\blog\entwin\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
